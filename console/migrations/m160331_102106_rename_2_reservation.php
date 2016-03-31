3abd713ca4ba6998d32eb2eacb388674e9a9a980
Brook Yang <uedsky@gmail.com>
2016-03-31 18:23:37 +0800

    add

---
+++ console/migrations/m160331_102106_rename_2_reservation.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+class m160331_102106_rename_2_reservation extends Migration
+{
+    public function up()
+    {
+        $this->renameTable('reservations', 'reservation');
+    }
+
+    public function down()
+    {
+        $this->renameTable('reservation', 'reservations');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
