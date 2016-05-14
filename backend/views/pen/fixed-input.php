e866ae1ebe76eb12b219f16bfb7f329ec5b4aa49
Brook Yang <uedsky@gmail.com>
2016-05-14 16:36:20 +0800

    update img-placeholder

--- backend/views/pen/fixed-input.php
+++ backend/views/pen/fixed-input.php
@@ -7,21 +7,33 @@
 使用 scrollIntoView 修复手机端 fixed input 无法正确定位问题
 <style>
 #fixed_bar { background:#f0f0f0; position: fixed;left: 0; right: 0; bottom: 0; padding: 10px; }
-#fixed_bar input { border: solid 1px #ccc; width:100%; padding: 10px; }
+#fixed_bar.focused { position: absolute; }
 </style>
+<?php for($i = 0; $i < 100; $i++) {?>
+<div>line <?=$i?></div>
+<?php } ?>
 <div id="fixed_bar">
-    <input type="text" id="txt_msg"/>
+    Fill the form
+    <p><input type="text" id="txt_msg" class="form-control"/></p>
+    <input type="tel" class="form-control"/>
 </div>
 <script>
 $(function() {
     $(document.documentElement).data('require-touch', true);
     $('#txt_msg').focus(function() {
         var input = this;
-        for(var i = 0; i <= 5; i ++) {
-            setTimeout(function () {
-                input.scrollIntoView();
-            }, 200 * i);
+        // 输入法启动慢，要多次, 或者 setInterval 避免切换输入时高度改变
+        $(this).data('fixedTimer', setInterval(function () {
+            input.scrollIntoView();
+        }, 300));
+
+        for(var i = 1; i < 11; i++) {
+            setTimeout(function() {
+//                input.scrollIntoView();
+            }, i * 300);
         }
+    }).blur(function() {
+        clearInterval($(this).data('fixedTimer'));
     });
-})
+});
 </script>
